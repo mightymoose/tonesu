0eb065d65cf10e6334389e8a3137a6f4c5d3dcc5
mightymoose <mryan.morris@gmail.com>
2021-01-11 06:11:54 -0600

    feat(web-client): add a route for the ping component

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
@@ -16,7 +19,7 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser');
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
   worker.start();
 }
